8d39acba1c6b2945a1389a979aad7d67c8635282
Mai Duy <duy@zeus-2.local>
2018-02-04 14:29:45 -0600

    update

--- src/main/java/edu/mum/wap/model/Movie.java
+++ src/main/java/edu/mum/wap/model/Movie.java
@@ -3,9 +3,9 @@ package edu.mum.wap.model;
 public class Movie {
     private String id;
     private String description;
-    private String duation;
+    private String duration;
     private String name;
     private String imageLandscape;
     private String imagePortrait;
-
+    private String imdb;
 }
