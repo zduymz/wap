da5d40b57afc165ee815485585b9bbc6e32b902c
Mai Duy <duy@zeus-2.local>
2018-02-04 08:22:43 -0600

    initialize

---
+++ src/main/java/edu/mum/wap/model/Cinema.java
@@ -0,0 +1,11 @@
+package edu.mum.wap.model;
+
+import java.util.ArrayList;
+
+public class Cinema {
+    private String id;
+    private String address;
+    private String name;
+    private Integer chairs;
+    private ArrayList<ShowDate> showdates;
+}
---
+++ src/main/java/edu/mum/wap/model/Movie.java
@@ -0,0 +1,11 @@
+package edu.mum.wap.model;
+
+public class Movie {
+    private String id;
+    private String description;
+    private String duation;
+    private String name;
+    private String imageLandscape;
+    private String imagePortrait;
+
+}
