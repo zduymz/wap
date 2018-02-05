bc7b54c62df8873e0484a38c9bc3a81db7c94540
gonnix <triallaga3@gmail.com>
2018-02-04 20:29:21 -0600

    update server side

--- src/main/java/edu/mum/wap/model/Movie.java
+++ src/main/java/edu/mum/wap/model/Movie.java
@@ -8,4 +8,70 @@ public class Movie {
     private String imageLandscape;
     private String imagePortrait;
     private String imdb;
+
+    public Movie(String id, String description, String duration, String name, String imageLandscape, String imagePortrait, String imdb) {
+        this.id = id;
+        this.description = description;
+        this.duration = duration;
+        this.name = name;
+        this.imageLandscape = imageLandscape;
+        this.imagePortrait = imagePortrait;
+        this.imdb = imdb;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageLandscape() {
+        return imageLandscape;
+    }
+
+    public void setImageLandscape(String imageLandscape) {
+        this.imageLandscape = imageLandscape;
+    }
+
+    public String getImagePortrait() {
+        return imagePortrait;
+    }
+
+    public void setImagePortrait(String imagePortrait) {
+        this.imagePortrait = imagePortrait;
+    }
+
+    public String getImdb() {
+        return imdb;
+    }
+
+    public void setImdb(String imdb) {
+        this.imdb = imdb;
+    }
 }
