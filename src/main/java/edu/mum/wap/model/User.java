203ec92a1c26fe08896c78138f9b6a482dea593f
gonnix <triallaga3@gmail.com>
2018-02-04 20:30:08 -0600

    update server side

--- src/main/java/edu/mum/wap/model/User.java
+++ src/main/java/edu/mum/wap/model/User.java
@@ -1,4 +1,23 @@
 package edu.mum.wap.model;
 
 public class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean verify(String pass) {
+        return password.equals(pass);
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
