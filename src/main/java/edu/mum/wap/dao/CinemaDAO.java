829ca309ac5bc9764ea041d8adc278b359e2d06f
Duy Mai <kenvin.md@gmail.com>
2018-02-05 21:07:30 -0600

    update

--- src/main/java/edu/mum/wap/dao/CinemaDAO.java
+++ src/main/java/edu/mum/wap/dao/CinemaDAO.java
@@ -14,8 +14,10 @@ public class CinemaDAO {
     private List<Cinema> cinemasDAO = new ArrayList<>();
     private MovieDAO movies = new MovieDAO();
     {
-        Cinema c1 = new Cinema("1", "IO, 52557", "Galaxy1");
-        Cinema c2 = new Cinema("2", "CO, 51234", "Galaxy2");
+        Cinema c1 = new Cinema("1", "1215 Theatre Drive, Ottumwa, IA 52501", "CEC - Ottumwa 8 Theatre");
+        Cinema c2 = new Cinema("2", "831 Ave G, Fort Madison, IA 52627", "Fox Theater - Fort Madison");
+        Cinema c3 = new Cinema("3", "1602 Sycamore St, Iowa City, IA 52240", "Marcus Sycamore Cinema");
+        Cinema c4 = new Cinema("4", "550 S Gear Ave Suite #38, West Burlington, IA 52655\n", "CEC - Westland Mall 10");
         c1.addSchedule(new ShowDate("2/14/2018", "Wed", "14:30"),
                 movies.getMovieById("1"), new Seat());
         c1.addSchedule(new ShowDate("2/14/2018", "Wed", "16:30"),
@@ -24,8 +26,22 @@ public class CinemaDAO {
                 movies.getMovieById("1"), new Seat());
         c2.addSchedule(new ShowDate("2/14/2018", "Wed", "16:30"),
                 movies.getMovieById("1"), new Seat());
+        c3.addSchedule(new ShowDate("2/14/2018", "Wed", "14:30"),
+                movies.getMovieById("1"), new Seat());
+        c4.addSchedule(new ShowDate("2/14/2018", "Wed", "16:30"),
+                movies.getMovieById("1"), new Seat());
+        c2.addSchedule(new ShowDate("2/14/2018", "Wed", "14:30"),
+                movies.getMovieById("2"), new Seat());
+        c2.addSchedule(new ShowDate("2/14/2018", "Wed", "16:30"),
+                movies.getMovieById("3"), new Seat());
+        c3.addSchedule(new ShowDate("2/14/2018", "Wed", "14:30"),
+                movies.getMovieById("2"), new Seat());
+        c4.addSchedule(new ShowDate("2/14/2018", "Wed", "16:30"),
+                movies.getMovieById("3"), new Seat());
         cinemasDAO.add(c1);
         cinemasDAO.add(c2);
+        cinemasDAO.add(c3);
+        cinemasDAO.add(c4);
     }
 
     public List<Cinema> getCinemaList(Movie movie) {
---
+++ web/resources/js/booking.js
@@ -0,0 +1,37 @@
+$(function(){
+
+    function updateSeat(){
+        $("#seat").text("");
+        $(".seat-col li[class=active]").each(function(){
+            let self = $(this);
+            let seatR = self.parents("[class=seat-row]").find("li[class=index]").first().text();
+            let seatN = self.text();
+            let currentSeat = $("#seat").text();
+            let newValue = currentSeat + ", " + seatR + seatN;
+            $("#seat").text(newValue.replace(/^,/,""));
+        })
+    }
+
+    function updatePrice() {
+        // need to do
+    }
+
+    $(".seat-col li").not("[class=selected]").click(function(){
+        let self = $(this);
+        let seatNo = parseInt($("#seatNo").val());
+        if (self.attr("class") == "active") {
+            $("#seatNo").val(seatNo + 1);
+            self.removeClass("active");
+            updateSeat();
+            updatePrice();
+            return;
+        }
+        if (seatNo > 0) {
+            $("#seatNo").val(seatNo - 1);
+            self.addClass("active");
+            updateSeat();
+            updatePrice();
+        }
+    })
+})
+
